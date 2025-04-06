9b56cae3f2a139c7642510245e582616a7451fbf
Dorian88 <doaljari@gmail.com>
2025-04-05 19:16:25 -0500

    finalizacion de Servlets modelo vista controlador CRUD

--- .metadata/.plugins/org.eclipse.wst.server.core/tmp0/wtpwebapps/WebDeProductos/ListaProductos.jsp
+++ .metadata/.plugins/org.eclipse.wst.server.core/tmp0/wtpwebapps/WebDeProductos/ListaProductos.jsp
@@ -22,6 +22,18 @@
 				text-align: center;
 				background-color: #5882FA;
 			}
+			
+			table{
+			
+				float: left;
+			
+			}
+			
+			#contenedorBoton{
+			
+				margin-left: 1000px;
+			
+			}
 		
 		</style>
 	</head>
@@ -39,10 +51,28 @@
 				<td class="cabecera">Precio</td>
 				<td class="cabecera">Importado</td>
 				<td class="cabecera">País de Origen</td>
+				<td class="cabecera">Acción</td>
 			
 			</tr>
 			
 			<c:forEach var="tempProd" items="${LISTAPRODUCTOS }">
+					<!-- Link para cada producto con su campo clave -->
+					
+					<c:url var="linkTemp" value="ControladorProductos">
+					
+						<c:param name="instruccion" value="cargar"></c:param>
+						<c:param name="CArticulo" value="${tempProd.cArt}"></c:param>
+					
+					</c:url>
+					
+					<!-- Link para eliminar un producto -->
+					<c:url var="linkTempEliminar" value="ControladorProductos">
+					
+						<c:param name="instruccion" value="eliminar"></c:param>
+						<c:param name="CArticulo" value="${tempProd.cArt}"></c:param>
+					
+					</c:url>
+					
 					<tr>
 			
 						<td class="filas">${tempProd.cArt}</td>
@@ -52,12 +82,20 @@
 						<td class="filas">${tempProd.precio}</td>
 						<td class="filas">${tempProd.importado}</td>
 						<td class="filas">${tempProd.pOrigen}</td>
+						<td class="filas"><a href="${linkTemp}">Actualizar</a>&nbsp;&nbsp;<a href="${linkTempEliminar}">Eliminar</a></td>
 			
 					</tr>
 					
 			</c:forEach>
 		
 		</table>
+		
+		<div id="contenedorBoton">
+			
+			
+			<input type="button" value="Insertar Registro" onclick="window.location.href='insertaProducto.jsp'"/>
+			
+		</div>
 		
 	</body>
 </html>
--- WebDeProductos/src/main/java/com/dorian/productos/ModeloProductos.java
+++ WebDeProductos/src/main/java/com/dorian/productos/ModeloProductos.java
@@ -1,6 +1,7 @@
 package com.dorian.productos;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
@@ -57,6 +58,158 @@ public class ModeloProductos {
 
 		}
 		return productos;
+	}
+
+	public void agregarElNuevoProducto(Productos nuevoProducto) throws Exception{
+		
+		Connection miConexion = null;
+		PreparedStatement miStatement = null;
+		 
+		//Obtener la conexion con la BD
+		try {
+			
+			miConexion = origenDatos.getConnection();
+			
+		//Crear la instruccion sql. Crear la consulta preparada (Statement)
+		String sql = "INSERT INTO PRODUCTOS (CÓDIGOARTÍCULO, SECCIÓN, NOMBREARTÍCULO, PRECIO, FECHA, IMPORTADO, PAÍSDEORIGEN)" + 
+						"VALUES(?,?,?,?,?,?,?)";
+		miStatement = miConexion.prepareStatement(sql);
+		
+		//Establecer parametros del producto
+		miStatement.setString(1, nuevoProducto.getcArt());
+		miStatement.setString(2, nuevoProducto.getSeccion());
+		miStatement.setString(3, nuevoProducto.getnArt());
+		miStatement.setString(4, nuevoProducto.getPrecio());
+		miStatement.setString(5, nuevoProducto.getFecha());
+		miStatement.setString(6, nuevoProducto.getImportado());
+		miStatement.setString(7, nuevoProducto.getpOrigen());
+		
+		//ejecutar la instruccion sql
+		miStatement.execute();
+		
+		}catch(Exception e) {
+			
+			e.printStackTrace();
+		}finally {
+			
+			miStatement.close();
+			miConexion.close();
+		}
+		
+	}
+
+	public Productos getProducto(String codigoArticulo) {
+		
+		Productos elProducto = null;
+		Connection miConexion = null;
+		PreparedStatement miStatement = null;
+		ResultSet miResultset = null;
+		String cArticulo = codigoArticulo;
+		
+		try {
+			
+			//Establecer la conexion con la BD
+			miConexion = origenDatos.getConnection();
+		
+			//Crear el SQL que busque el producto
+			String sql = "SELECT * FROM PRODUCTOS WHERE CÓDIGOARTÍCULO=?";
+		
+			//Crear la consulta preparada
+			miStatement = miConexion.prepareStatement(sql);
+		
+			//Establecer los parametros
+			miStatement.setString(1, cArticulo);
+		
+			//Ejecutar la consulta
+			miResultset = miStatement.executeQuery();
+		
+			//Obtener los datos de respuesta
+			if(miResultset.next()) {
+				
+				String cArt = miResultset.getString("CÓDIGOARTÍCULO");
+				String seccion = miResultset.getString("SECCIÓN");
+				String nArt = miResultset.getString("NOMBREARTÍCULO");
+				String precio = miResultset.getString("PRECIO");
+				String fecha = miResultset.getString("FECHA");
+				String importado = miResultset.getString("IMPORTADO");
+				String pOrig = miResultset.getString("PAÍSDEORIGEN");
+				
+				elProducto = new Productos(cArt, seccion, nArt, precio, fecha, importado, pOrig);
+				
+			}else {
+				
+				throw new Exception("No se ha encontrado con código artículo " + cArticulo);
+			}
+			
+			}catch(Exception e) {
+			
+				e.printStackTrace();
+			
+			}
+		
+		return elProducto;
+	}
+
+	public void actualizarProducto(Productos productoActualizado) throws Exception{
+		
+		Connection miConexion = null;
+		PreparedStatement miStatement = null;
+				
+		//Establecer la conexion con la BD
+		try {
+			miConexion = origenDatos.getConnection();
+		
+			//Crear el SQL que busque el producto
+			String sql = "UPDATE PRODUCTOS SET SECCIÓN=?, NOMBREARTÍCULO=?, PRECIO=?, FECHA=?, IMPORTADO=?, PAÍSDEORIGEN=? WHERE CÓDIGOARTÍCULO=?";
+		
+			//Crear la consulta preparada
+			miStatement = miConexion.prepareStatement(sql);
+		
+			//Establecer los parametros
+			miStatement.setString(1, productoActualizado.getSeccion());
+			miStatement.setString(2, productoActualizado.getnArt());
+			miStatement.setString(3, productoActualizado.getPrecio());
+			miStatement.setString(4, productoActualizado.getFecha());
+			miStatement.setString(5, productoActualizado.getImportado());
+			miStatement.setString(6, productoActualizado.getpOrigen());
+			miStatement.setString(7, productoActualizado.getcArt());
+		
+			//Ejecutar la consulta
+			miStatement.execute();
+			}finally {
+				
+				miStatement.close();
+				miConexion.close();
+			}
+		
+	}
+
+	public void eliminarProducto(String codArticulo) throws Exception{
+		
+		Connection miConexion = null;
+		PreparedStatement miStatement = null;
+				
+		//Establecer la conexion con la BD
+		try {
+			miConexion = origenDatos.getConnection();
+		
+			//Crear instruccion SQL de eliminacion
+			String sql = "DELETE FROM PRODUCTOS WHERE CÓDIGOARTÍCULO=?";
+		
+			//Preparar la consulta
+			miStatement = miConexion.prepareStatement(sql);
+		
+			//establecer parametros de la consulta
+			miStatement.setString(1, codArticulo);
+		
+			//ejecutar la sentencia SQL
+			miStatement.execute();
+			
+		}finally {
+			
+			miStatement.close();
+			miConexion.close();
+		}
 	}
 
 }
